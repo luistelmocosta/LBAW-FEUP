ea7ca8d2928dcf348b787c017680d24dc1166abf
luistelmocosta <luistelmocosta@gmail.com>
2017-03-09 00:18:57 +0000

    Database started :dash:

--- config/config.php
+++ config/config.php
@@ -16,6 +16,32 @@ $smarty->template_dir = '../templates';
 $smarty->compile_dir = '../templates_c';
 $smarty->assign('BASE_URL', $BASE_URL);
 
+try {
+
+
+    $dbuser = 'luiscosta';
+    $dbpass = '123';
+    $host = 'localhost';
+    $dbname = 'lbaw';
+    $conn = new PDO("pgsql:host=$host;dbname=$dbname", $dbuser, $dbpass);
+}catch (PDOException $e) {
+    echo "Error : " . $e->getMessage() . "<br/>";
+    die();
+}
+
+$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$conn->exec('SET SCHEMA \'public\'');
+
+$sql = 'SELECT * FROM User';
+
+
+if (!$conn) {
+    echo "Error : Unable to open database\n";
+} else {
+    echo "Opened database successfully\n";
+}
+
 
 
 
