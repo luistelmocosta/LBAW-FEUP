65a6f6a75a8c724637ea251f6c0618185ebb17ab
LuísCosta <luistelmocosta@gmail.com>
2017-03-27 00:20:25 +0100

    Added Artefact 6 structure

---
+++ database_versions/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO "User" (username, email, password)
+VALUES ('editor', 'editor@editor.pt', '12345678');
+INSERT INTO "User" (username, email, password)
+VALUES ('luistelmocosta', 'luistelmocosta@gmail.com', '12345678');
