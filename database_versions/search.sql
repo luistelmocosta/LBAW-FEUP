647a35b7f88d9a39a777e9c522596f92ec29c14e
LuísCosta <luistelmocosta@gmail.com>
2017-03-30 14:28:11 +0100

    FTS trigger working :wine_glass:

---
+++ database_versions/search.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION search_questions(psearch text)
+  RETURNS TABLE (questionid INTEGER) AS $func$
+BEGIN
+  return QUERY
+  SELECT DISTINCT publications.publicationid
+  FROM questions, publications
+  WHERE to_tsvector(coalesce(questions.title,'') || ' ' || coalesce(publications.body,'')) @@ to_tsquery(psearch)
+        OR questionid IN (
+    SELECT DISTINCT(answers.questionid) FROM answers INNER JOIN publications ON answers.publicationid = publications.publicationid
+    WHERE to_tsvector(coalesce(publications.body)) @@ to_tsquery(psearch)
+  )
+  ;
+END
+$func$  LANGUAGE plpgsql;
