5c68ab8931fbe934eab1e104246950c327505c75
LuísCosta <luistelmocosta@gmail.com>
2017-04-19 20:36:24 +0100

    Gnome config

--- config/init.php
+++ config/init.php
@@ -3,18 +3,22 @@
 //This file is included in every .php file
 session_start();
 
+//LOCAL
+//$BASE_DIR = '/home/luiscosta/PhpstormProjects/LBAW-FEUP/'; //CHANGE WITH YOURS
+//$BASE_URL = '/';
 
-$BASE_DIR = '/home/luiscosta/PhpstormProjects/LBAW-FEUP/'; //CHANGE WITH YOURS
-$BASE_URL = '/';
+//GNOME
+$BASE_DIR = '/opt/lbaw/lbaw1641/public_html/proto/'; //FIXME
+$BASE_URL = '/~lbaw1616/proto/'; //FIXME
 
 
 //Connect to the database
 
 try {
-    $dbuser = 'luiscosta'; //CHANGE WITH YOUR INFO
-    $dbpass = '123';
-    $host = 'localhost';
-    $dbname = 'lbaw';
+    $dbuser = 'lbaw1641'; //CHANGE WITH YOUR INFO
+    $dbpass = 'zl50rg14';
+    $host = 'dbm.fe.up.pt';
+    $dbname = 'lbaw1641';
     $conn = new PDO("pgsql:host=$host;dbname=$dbname", $dbuser, $dbpass);
 }catch (PDOException $e) {
     echo "Error : " . $e->getMessage() . "<br/>";
