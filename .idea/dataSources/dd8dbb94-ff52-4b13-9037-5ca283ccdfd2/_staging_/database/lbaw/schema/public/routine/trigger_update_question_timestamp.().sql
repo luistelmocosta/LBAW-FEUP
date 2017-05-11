da0e9cd96bd74597984356dc3ca524255ccc0937
LuísCosta <luistelmocosta@gmail.com>
2017-05-11 01:45:39 +0100

    .idea removed

--- .idea/dataSources/dd8dbb94-ff52-4b13-9037-5ca283ccdfd2/_staging_/database/lbaw/schema/public/routine/trigger_update_question_timestamp.().sql
+++
@@ -1,8 +0,0 @@
-create function trigger_update_question_timestamp() returns trigger
-LANGUAGE plpgsql
-AS $$
-BEGIN
-  UPDATE publications SET last_edit_date = now() WHERE publicationid = NEW.publicationid;
-  RETURN NULL;
-END;
-$$;
