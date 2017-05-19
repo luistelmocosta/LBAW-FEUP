d46a048845731700f713b60aabf5f1a1973b4591
LuísCosta <luistelmocosta@gmail.com>
2017-05-20 00:29:35 +0100

    Categories done

--- database_versions/lbaw_v1.sql
+++ database_versions/lbaw_v1.sql
@@ -854,37 +854,37 @@ END
 $func$  LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION category_questions(skip INTEGER, limitNumber INTEGER, cid INTEGER)
-    RETURNS TABLE (
-        publicationid INTEGER,
-        name VARCHAR,
-        title VARCHAR(100),
-        body TEXT,
-        creation_date TIMESTAMP,
-        solved_date TIMESTAMP,
-        username VARCHAR(10),
-        userid INTEGER,
-        answers_count BIGINT,
-        upvotes BIGINT,
-        votes_count BIGINT,
-        views_counter BIGINT)
+  RETURNS TABLE (
+    publicationid INTEGER,
+    name VARCHAR,
+    title VARCHAR(100),
+    body TEXT,
+    creation_date TIMESTAMP,
+    solved_date TIMESTAMP,
+    username VARCHAR(10),
+    userid INTEGER,
+    answers_count BIGINT,
+    upvotes BIGINT,
+    votes_count BIGINT,
+    views_counter BIGINT)
 AS $func$
 BEGIN
-    RETURN QUERY
-    SELECT questions.publicationid, categories.name, questions.title, publications.body,
-        publications.creation_date, questions.solved_date, users.username, users.userid,
-        (SELECT COUNT(*) FROM question_answers(questions.publicationid)) AS answers_count,
-        (SELECT COUNT (*) FROM votes WHERE votes.values = 1 AND votes.publicationid = 1) AS upvotes,
-        (SELECT SUM(votes.values) FROM votes WHERE votes.publicationid = questions.publicationid),
-        questions.views_counter
-    FROM questions
-        INNER JOIN publications
-            ON questions.publicationid = publications.publicationid
-        INNER JOIN categories ON questions.categoryid = categories.categoryid
-        LEFT JOIN users ON publications.userid = users.userid
-    WHERE questions.categoryid = cid
-    ORDER BY creation_date DESC
-    LIMIT limitNumber
-    OFFSET skip;
+  RETURN QUERY
+  SELECT questions.publicationid, categories.name, questions.title, publications.body,
+    publications.creation_date, questions.solved_date, users.username, users.userid,
+    (SELECT COUNT(*) FROM question_answers(questions.publicationid)) AS answers_count,
+    (SELECT COUNT (*) FROM votes WHERE votes.values = 1 AND votes.publicationid = 1) AS upvotes,
+    (SELECT SUM(votes.values) FROM votes WHERE votes.publicationid = questions.publicationid),
+    questions.views_counter
+  FROM questions
+    INNER JOIN publications
+      ON questions.publicationid = publications.publicationid
+    INNER JOIN categories ON questions.categoryid = categories.categoryid
+    LEFT JOIN users ON publications.userid = users.userid
+  WHERE questions.categoryid = cid
+  ORDER BY creation_date DESC
+  LIMIT limitNumber
+  OFFSET skip;
 END
 $func$  LANGUAGE plpgsql;
 
